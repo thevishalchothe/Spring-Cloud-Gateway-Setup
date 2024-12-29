9f397e178ad9d7602ffe3aba13a6e214398b338a
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2024-12-29 18:35:07 +0530

    Created Spring Cloud API Gateway 🌟🛠️

---
+++ SpringCloudAPIGateway/src/test/java/com/springcloud/apigetway/SpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.apigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
